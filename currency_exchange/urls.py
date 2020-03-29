3854fb1632f5c9e759c688c22ce3c939d3bda86b
Pavlo Mykhalichenko <pavlom@oneplanetops.com>
2020-03-29 08:29:24 +0300

    master - restframework lesson

--- currency_exchange/urls.py
+++ currency_exchange/urls.py
@@ -3,6 +3,15 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf import settings
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_swagger.views import get_swagger_view
+
+
+API_PREFIX = 'api/v1'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +19,18 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('currency/', include('currency.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+
+    # API
+    path(f'{API_PREFIX}/currency/', include('currency.api.urls')),
+    path(f'{API_PREFIX}/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(f'{API_PREFIX}/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
+# SWAGGER
+schema_view = get_swagger_view(title='DOCS')
+
+urlpatterns.append(path(f'{API_PREFIX}/docs/', schema_view))
 
 
 if settings.DEBUG:
